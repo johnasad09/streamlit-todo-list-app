5142cf7cbdc489c7798646fe5feeb5b45ce68efd
[Asad Ullah] <johnasad09@gmail.com>
2026-01-01 01:15:10 +0500

    Add joke of the day functionality

--- todo_list.py
+++ todo_list.py
@@ -1,9 +1,15 @@
 import streamlit as st
 from task import Task
+from jokes import get_joke
 
 if "task_list" not in st.session_state:
     st.session_state.task_list = []
 task_list = st.session_state.task_list
+
+if "joke" not in st.session_state:
+    api_key = st.secrets["jokes_api"]["api_key"]
+    st.session_state.joke = get_joke(api_key)
+
 
 def add_task(task_name: str):
     task_list.append(Task(task_name))
@@ -24,6 +30,7 @@ with st.sidebar:
     if st.button("Add Task", type="primary"):
         add_task(task)
 
+st.info(st.session_state.joke)
 
 total_tasks = len(task_list)
 completed_tasks = sum(1 for task in task_list if task.is_done)
